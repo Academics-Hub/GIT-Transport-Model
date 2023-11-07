26370eac419df64d45627f569bc7e150e804bb32
Jonathan faller <jono.faller@gmail.com>
2023-11-07 22:04:56 +0200

    testing update all branches hook

--- .github/custom-scripts/update-branches.sh
+++ .github/custom-scripts/update-branches.sh
@@ -1,15 +1,11 @@
 #!/bin/env bash
-mainBranch=main
-# finds all branches
-branches=$(git branch | grep -v 'main')
-
 function helper_git() {
 	git pull
 	git checkout "$1"
   	git merge --no-edit $mainBranch
 	git push
 }
-
+mainBranch=main
+branches=$(git branch | grep -v 'main')
 parallel --jobs $(nproc) helper_git {} :::: <(echo "$branches")
-# finish by checking out to main
 git checkout $mainBranch
