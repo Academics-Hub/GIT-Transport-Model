b07b91c94a98db62c72b2fbaea0f580eda387a9f
Jonathan faller <jono.faller@gmail.com>
2023-11-12 19:47:13 +0200

    got rid of the new folders, they make dealing with matlab paths weird

--- Code/src/Glucose.m
+++ Code/src/Glucose.m
@@ -1,4 +1,4 @@
 % function which uses all the sources & absorptions of glucose to output an overall glucose concentration
-function [glucose] = Glucose(carbs, lipids, protein, fibre, time_step)
+function [glucose] = Glucose(time_step)
 
 end
--- Code/src/delta_gut.m
+++ Code/src/delta_gut.m
@@ -1,5 +1,7 @@
 function [Gut_new] = delta_gut(Glucose,SpO2,Insulin,GutFlowRate,time_step)
-
+    % change in glucose in gut -> absorption
+    % change in glucose output
+    Gut_new.Glucose_out = Glucose(time_step);
     
-    Gut_new = Gut_old;
+    
 end
