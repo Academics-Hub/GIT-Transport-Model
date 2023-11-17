d96b13cc60db58e01fa37e3c4f4c2332f2f5de03
JustMeJuls <julia.m.gaylard@gmail.com>
2023-11-17 10:05:24 +0200

    actually changed output

--- Code/src/O2_Absorption.m
+++ Code/src/O2_Absorption.m
@@ -6,7 +6,7 @@
 % Cb: oxygen carrying capacity (mg/g)
 % Hb: haemoglobin concentration (g/l)
 
-function [gut_spO2] = O2_Absorption(GutFlowRate, arterial_spO2, Cb, Hb, O2_usage)
+function [gut_O2] = O2_Absorption(GutFlowRate, arterial_spO2, Cb, Hb, O2_usage)
 
 % arterial_spO2 - arterial_spO2_new = 15 for fasting?? (need to check)
 % are we deciding on a percentage of usage or bringing in a venous
@@ -15,6 +15,6 @@ function [gut_spO2] = O2_Absorption(GutFlowRate, arterial_spO2, Cb, Hb, O2_usage
 % O2_usage = 0.15
 
 % arterial_spO2_new = arterial_spO2 - O2_usage;
-gut_spO2 = 02_usage * GutFlowRate * Hb * Cb;  
+gut_O2 = 02_usage * GutFlowRate * Hb * Cb;  
 
 end
