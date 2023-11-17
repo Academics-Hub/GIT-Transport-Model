71d5593e5506e61d3ba8f780fa558f9f4cb5cff7
Dinal Nagar <nagardinal@gmail.com>
2023-11-17 17:55:25 +0200

    cleaned up code and removed redundancies

--- Code/src/glucose_absorption.m
+++ Code/src/glucose_absorption.m
@@ -1,15 +1,13 @@
 function [GutOut_glucose,GutNew_glucose] = glucose_absorption(vmax, km, arterialglucose, gutglucose, GutFlowRate, step)
 
     % Michaelis-Menten kinetics for glucose metabolism
-    glucose_usage = vmax * Gut.Glucose / (km + gutglucose); 
-    glucose_usage = vmax * Gut.Glucose / (km + gutglucose);
+    glucose_usage = vmax * gutglucose / (km + gutglucose); 
 
     % Update using the metabolism rate
-    GutNew_glucose = Gut.Glucose - (step * GutFlowRate) * glucose_usage; 
+    GutNew_glucose = gutglucose + (step * GutFlowRate) * glucose_usage;
 
     % Update glucose using the metabolism rate
-    GutNew_glucose = arterialglucose + (step * GutFlowRate) * glucose_usage;
-    GutOut_glucose = gutglucose - (step * GutFlowRate) * glucose_usage;
+    GutOut_glucose = arterialglucose - (step * GutFlowRate) * glucose_usage;
     
 end
 
