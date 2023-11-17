bee98fc572a8853e7a9c9cbbff366a938022b74d
Dinal Nagar <nagardinal@gmail.com>
2023-11-17 23:58:32 +0200

    Update glucose_aborption_2.m

--- Code/src/glucose_aborption_2.m
+++ Code/src/glucose_aborption_2.m
@@ -1,24 +1,24 @@
 function [GutOut_glucose,GutNew_glucose] = glucose_absorption(vmax, km, arterialglucose, gutglucose, GutFlowRate, insulin, step, P1, P2, G0, Ib)
 
     %normal basal values 
-    Gb = 5.6 ; %fssting glcuose mmol/L
-    Ib = 174 ; %fasting insulin pmol/L
+    Gb = 5.6; %fssting glcuose mmol/L
+    Ib = 174; %fasting insulin pmol/L
+    Sg = 0.033; 
 
     % X(t) is the interstitial insulin at time t. assumed to be the blood insulin
-    X = insulin  ; 
+    X = insulin; 
 
     % Define the differential equations
-    dG_dt = -[P1 + X]*gutglucose + P1*Gb;
-    dX_dt = -P2*X + P1*(insulin - Ib);
+    dG_dt = -[Sg + X]*gutglucose + Sg*Gb;
 
     % Update glucose usage using the differential equations
-    glucose_usage = dG_dt * step;
+    glucose_change_plasma = dG_dt * step;
 
     % Update using the metabolism rate
-    GutNew_glucose = gutglucose + (step * GutFlowRate) * glucose_usage;
+    GutNew_glucose = gutglucose + (step * GutFlowRate) * glucose_change_plasma;
 
     % Update glucose using the metabolism rate
-    GutOut_glucose = arterialglucose - (step * GutFlowRate) * glucose_usage;
+    GutOut_glucose = arterialglucose - (step * GutFlowRate) * glucose_change_plasma;
     
 end
 
