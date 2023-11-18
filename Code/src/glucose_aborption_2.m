cc47eba192e736cf14c55c6fb44c330b180d2b44
Dinal Nagar <nagardinal@gmail.com>
2023-11-18 11:43:12 +0200

    new calculation for glucose absorption

--- Code/src/glucose_aborption_2.m
+++ Code/src/glucose_aborption_2.m
@@ -1,9 +1,9 @@
-function [GutOut_glucose,GutNew_glucose] = glucose_absorption(vmax, km, arterialglucose, gutglucose, GutFlowRate, insulin, step, P1, P2, G0, Ib)
+function [GutOut_glucose,GutNew_glucose] = glucose_absorption(arterialglucose, gutglucose, GutFlowRate, insulin, step)
 
     %normal basal values 
     Gb = 5.6; %fssting glcuose mmol/L
     Ib = 174; %fasting insulin pmol/L
-    Sg = 0.033; 
+    Sg = 0.033; %net glucose utilization without dynamic insulin response min^-1
 
     % X(t) is the interstitial insulin at time t. assumed to be the blood insulin
     X = insulin; 
@@ -14,11 +14,14 @@ function [GutOut_glucose,GutNew_glucose] = glucose_absorption(vmax, km, arterial
     % Update glucose usage using the differential equations
     glucose_change_plasma = dG_dt * step;
 
+    % Michaelis-Menten kinetics for glucose metabolism
+    glucose_usage = vmax * gutglucose / (km + gutglucose); 
+
     % Update using the metabolism rate
     GutNew_glucose = gutglucose + (step * GutFlowRate) * glucose_change_plasma;
 
     % Update glucose using the metabolism rate
-    GutOut_glucose = arterialglucose - (step * GutFlowRate) * glucose_change_plasma;
+    GutOut_glucose = arterialglucose + (step * GutFlowRate) * glucose_change_plasma;
     
 end
 
