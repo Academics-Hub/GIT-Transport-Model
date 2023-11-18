2efb9a2bece228185270310e711456dd9c039ee8
Dinal Nagar <nagardinal@gmail.com>
2023-11-18 23:50:59 +0200

    bergson minimal model update

--- Code/src/glucose_aborption_2.m
+++ Code/src/glucose_aborption_2.m
@@ -2,23 +2,22 @@ function [GutOut_glucose,GutNew_glucose] = glucose_absorption(arterialglucose, g
 
     %normal basal values 
     Gb = 5.6; %fssting glcuose mmol/L
-    %Ib = 174; %fasting insulin pmol/L
-    Sg = 0.033; %net glucose utilization without dynamic insulin response min^-1
+    Sg = 0.035; %net glucose utilization without dynamic insulin response min^-1
 
     % X(t) is the interstitial insulin at time t. assumed to be the blood insulin
     X = insulin; 
 
     % Define the differential equations for minimal model
-    dG_dt = -[Sg + X]*gutglucose + Sg*Gb;
+    dG_dt = -(Sg + X)*gutglucose + Sg*Gb + arterialglucose/gutglucose;
 
     % Update glucose usage using the differential equations
-    glucose_change_plasma = dG_dt * step;
+    glucose_change_plasma = dG_dt * step * GutFlowRate;
 
     % Update using the metabolism rate
-    GutNew_glucose = gutglucose + (step * GutFlowRate) * glucose_change_plasma;
+    GutNew_glucose = gutglucose +  glucose_change_plasma;
 
     % Update glucose using the metabolism rate
-    GutOut_glucose = arterialglucose + (step * GutFlowRate) * glucose_change_plasma;
+    GutOut_glucose = arterialglucose +  glucose_change_plasma;
     
 end
 
--- Code/src/glucose_absorption_3.m
+++ Code/src/glucose_absorption_3.m
@@ -1,9 +1,9 @@
-function [GutOut_glucose,GutNew_glucose] = glucose_absorption(arterialglucose, gutglucose, GutFlowRate, insulin, step)
+function [GutOut_glucose,GutNew_glucose,J_glucose] = glucose_absorption(arterialglucose, gutglucose, GutFlowRate, insulin, step)
 
     A = 300; % define surface area of gut in m^2
-    D= 5.7e-10; %define diffusibility
+    D = 5.7e-10; %define diffusibility 
     thickeness = 0.005; % small intestin is 5mm thick
-    partition_coef = 0.65; 
+    partition_coef = 6.5e-5; %m^2 / sec
     K = D * partition_coef / thickeness; %calculate permeability
 
     J_glucose = K * A * (gutglucose - arterialglucose);
