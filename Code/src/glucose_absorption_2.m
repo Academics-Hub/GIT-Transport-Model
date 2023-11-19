3ca17d674d28239c0c32fed10f944a71725aee1b
Dinal Nagar <nagardinal@gmail.com>
2023-11-19 18:05:03 +0200

    updated testing.m for new glucose function

--- Code/src/glucose_absorption_2.m
+++ Code/src/glucose_absorption_2.m
@@ -1,14 +1,14 @@
 function [glucose_change_plasma] = glucose_absorption_2(gutglucose, GutFlowRate, insulin, glucoseoutput, step) %need glucoseoutput from jono
 
     %normal basal values 
-    Gb = 5.6; %fssting glcuose mmol/L
+    Gb = 5.6; %fasting glcuose mmol/L
     Sg = 0.035; %net glucose utilization without dynamic insulin response min^-1
 
     % X(t) is the interstitial insulin at time t. assumed to be the blood insulin
     X = insulin; 
 
     % Define the differential equations for minimal model
-    dG_dt = -(Sg + X)*gutglucose + Sg*Gb + glucoseoutput/gutglucose;
+    dG_dt = -(Sg + X)*gutglucose + Sg*Gb + glucoseoutput/300;
 
     % Update glucose usage using the differential equations
     glucose_change_plasma = dG_dt * step * GutFlowRate;
@@ -17,7 +17,7 @@ function [glucose_change_plasma] = glucose_absorption_2(gutglucose, GutFlowRate,
     %GutNew_glucose = gutglucose + glucose_change_plasma;
 
     % Update glucose using the metabolism rate
-   % GutOut_glucose = arterialglucose + glucose_change_plasma;
+   %GutOut_glucose = arterialglucose + glucose_change_plasma;
 end
 
 
--- Code/src/testing.m
+++ Code/src/testing.m
@@ -2,11 +2,13 @@ clear
 clc
 cla
 
-% main script to test functions
+% main script to test individual functions
 
 % testing overall model things
 GutFlowRate = 500; %ml / min
 GutFlowRate = GutFlowRate /1000;
+
+glucoseoutput = GUT_PARAMS.setget_glucose_output;
 
 % initializing Arterial things
 ArterialSpO2 = 0.98; 
@@ -23,28 +25,28 @@ duration = 24 * 3600; % 24 hours in seconds
 time = 0:step:duration;
 
 % Initialize arrays to store results
-GutOut_glucose_values = zeros(size(time));
-GutNew_glucose_values = zeros(size(time));
+glucose_change_plasma_values = zeros(size(time));
+%GutNew_glucose_values = zeros(size(time));
 %J_glucose_values = zeros(size(time));
 
 % Run the simulation
 for i = 1:length(time)
-    [GutOut_glucose, GutNew_glucose] = glucose_absorption(ArterialGlucose, 1, GutFlowRate, ArterialInsulin, step);
+    [glucose_change_plasma] = glucose_absorption_2(1, GutFlowRate, ArterialInsulin, glucoseoutput, step);
     
     % Store results
-    GutOut_glucose_values(i) = GutOut_glucose;
-    GutNew_glucose_values(i) = GutNew_glucose;
+    %GutOut_glucose_values(i) = GutOut_glucose;
+    glucose_change_plasma_values(i) = glucose_change_plasma;
     %J_glucose_values(i)= J_glucose;
     
     % Update Gut for the next iteration
-    Gut = [GutNew_glucose, 1]; % assuming 1 for simplicity, update as needed
+    Gut = [glucose_change_plasma, 1]; % assuming 1 for simplicity, update as needed
 end
 
 % Plot the results
 figure;
-plot(time, GutOut_glucose_values, 'b', 'LineWidth', 2);
+plot(time, glucoseoutput, 'b', 'LineWidth', 2);
 hold on;
-plot(time, GutNew_glucose_values, 'r', 'LineWidth', 2);
+%plot(time, GutNew_glucose_values, 'r', 'LineWidth', 2);
 xlabel('Time (seconds)');
 ylabel('Glucose Concentration');
 legend('Gut Out Glucose', 'Gut New Glucose');
