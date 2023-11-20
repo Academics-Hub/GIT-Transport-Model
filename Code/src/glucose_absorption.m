3b9dbcfc87ebd2b72884a720347fada2c6c7c8c4
Jonathan faller <jono.faller@gmail.com>
2023-11-20 03:31:36 +0200

    time for sleep

--- Code/src/glucose_absorption.m
+++ Code/src/glucose_absorption.m
@@ -1,4 +1,12 @@
-function [glucose_usage] = glucose_absorption(gutglucose, GutFlowRate, step)
+function [glucose_usage] = glucose_absorption(gutglucose, GutFlowRate, time_step)
+	% GutFlowRate is in mL/min 
+	% v is in mmole/s
+
+	% Gutflowrate -> L/(s/time_step)
+
+	GutFlowRate = GutFlowRate * 1000 / (60 * time_step); % L/time_step -> L/s
+	% mmol/s * s / L/s = mmol/L 
+
 
 	%glut 2
 	vmax =3; % mmole s -1
@@ -7,7 +15,8 @@ function [glucose_usage] = glucose_absorption(gutglucose, GutFlowRate, step)
 	% Michaelis-Menten kinetics for glucose metabolism
 	%glucose_usage = vmax * gutglucose / (km + gutglucose); 
 	%glucose_usage = (vmax * gutglucose / (km + gutglucose)) * (step * GutFlowRate); 
-	glucose_usage = (vmax * gutglucose / (km + gutglucose)) * step;
+	v = (vmax * gutglucose / (km + gutglucose));
+	glucose_usage = v * time_step / GutFlowRate; % someone needs to check the logic here, but this makes the units mmol/L
 	% Update using the metabolism rate
 	%GutNew_glucose = gutglucose + (step * GutFlowRate) * glucose_usage;
 
