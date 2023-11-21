97d4ae5f2fbffce3384b258abe0c4fed7d2564f8
JustMeJuls <julia.m.gaylard@gmail.com>
2023-11-21 21:24:18 +0200

    .

--- Code/src/O2_fed_fasting.m
+++ Code/src/O2_fed_fasting.m
@@ -1,7 +1,8 @@
 % amount of O2 absorbed from arterial (hopefully)
 
 % Inputs:
-% GutFlowRate: rate of blood flow to gut (ml/min)
+% GutFlowRate: rate of blood flow to gut (l/min)
+% arterial_spO2: oxygen saturation of entering blood (%)
 % Cb: oxygen carrying capacity (mg/g)
 % Hb: haemoglobin concentration (g/l)
 
@@ -19,47 +20,33 @@ function [gut_O2, gut_spO2, gut_CO2] = O2_fed_fasting(GutFlowRate, Cb, Hb)
     elseif time_since_last_meal > 12600
         O2_usage = (-1/3600) * time_since_last_meal + 4.15;
     end
-<<<<<<< HEAD
-    gut_spO2 = O2_usage
+    gut_spO2 = O2_usage;
     %O2_usage
 
     % arterial_spO2 - arterial_spO2_new = 15 for fasting?? (need to check)
 
     % O2_usage = 0.15
-=======
-    gut_spO2 = O2_usage;
->>>>>>> dd64029f65ea3de358f0dbb76bf668a7cf5cc688
 
     %arterial_spO2_new = arterial_spO2 - O2_usage;
 
-    gut_O2_mg = O2_usage * GutFlowRate * Hb * Cb;
-    % gut_O2_mg in mg/min
+    gut_O2 = O2_usage * GutFlowRate * Hb * Cb;
+    % gut_O2 in mg/min
 
     %change units to g/min
-    gut_O2_g = gut_O2_mg / 1000;
+    gut_O2_g = gut_O2 / 1000;
 
     %change units to mol
     %O2: 32 g/mol
-<<<<<<< HEAD
-    gut_O2_mol = gut_O2_g / 32
+    gut_O2_mol = gut_O2_g / 32;
     % conversion from mol/min mol/(s/time_step)
     gut_O2_mol = gut_O2_mol / (60/time_step);
     gut_O2 = gut_O2_mol;
-=======
-    gut_O2_mol = gut_O2_g / 32;
-    % conversion from mol/min to mol/(s*time_step)
-    gut_O2_mol_timestep = gut_O2_mol / (60*time_step);
-    gut_O2 = gut_O2_mol_timestep;
->>>>>>> dd64029f65ea3de358f0dbb76bf668a7cf5cc688
 
     %gut_O2_mol
 
-    gut_CO2_mol = gut_O2_mol_timestep;
+    gut_CO2_mol = gut_O2_mol;
     gut_CO2 = gut_CO2_mol;
 
     %CO2: 44 g/mol 
     %gut_CO2_g = gut_CO2_mol * 44;
-
-    %gut_CO2 = gut_CO2_g * 1000;
-
 end
